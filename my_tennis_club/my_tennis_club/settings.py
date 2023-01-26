8bb0d57e9363af7464cb7af62be3c66a641f4972
Ferdyplaza@hotmail.com <Ferdyplaza@hotmail.com>
2023-01-26 11:20:02 +0100

    9th commit

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5-@f$bb9iot=!ee$gn1+^2cc8nfbt7+fusdw5q+8*k3#7ez$xj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
